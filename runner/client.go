d56535c457ba7e6ebd198720fceb2a2a004f32b2
CHoU <handsomecheung@gmail.com>
2026-07-02 22:53:07 +0900

    release: bump version to v0.1.8 and add release notes

--- runner/client.go
+++ runner/client.go
@@ -96,7 +96,7 @@ func (c *Client) sendRegister() error {
 
 	payload := map[string]any{
 		"name":     c.name,
-		"version":  "0.1.7",
+		"version":  "0.1.8",
 		"hostname": hostname,
 		"os":       runtime.GOOS,
 		"arch":     runtime.GOARCH,
