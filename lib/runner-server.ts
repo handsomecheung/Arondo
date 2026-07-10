2078b6cdf6dc5b4ba4b17584c1f489791a044bf6
CHoU <handsomecheung@gmail.com>
2026-07-10 22:28:07 +0900

    release: bump version to v0.2.6 and add release notes

--- lib/runner-server.ts
+++ lib/runner-server.ts
@@ -69,7 +69,7 @@ export function setupRunnerServer(wss: WebSocketServer): void {
                 method: "connected",
                 payload: {
                   runnerId,
-                  serverVersion: "0.2.5",
+                  serverVersion: "0.2.6",
                   // Inform the runner which binaries to detect on its PATH.
                   // The runner responds with an agent.status event.
                   queryAgents: getAgentBinaryNames(),
