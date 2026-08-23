241631c7dd84d2dd47a89a9923343cb43978499c
CHoU <handsomecheung@gmail.com>
2026-08-23 16:14:21 +0900

    chore: release v0.2.23

--- runner/client.go
+++ runner/client.go
@@ -137,7 +137,7 @@ func (c *Client) sendRegister() error {
 	hostname, _ := os.Hostname()
 
 	payload := map[string]any{
-		"version":  "0.2.22",
+		"version":  "0.2.23",
 		"hostname": hostname,
 		"os":       runtime.GOOS,
 		"arch":     runtime.GOARCH,
