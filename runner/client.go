59b3b1061cb8f04434ae9292cc553ccdf1e23d28
CHoU <handsomecheung@gmail.com>
2026-09-11 11:05:50 +0900

    release: v0.2.39

--- runner/client.go
+++ runner/client.go
@@ -140,7 +140,7 @@ func (c *Client) sendRegister() error {
 	hostname, _ := os.Hostname()
 
 	payload := map[string]any{
-		"version":  "0.2.38",
+		"version":  "0.2.39",
 		"hostname": hostname,
 		"os":       runtime.GOOS,
 		"arch":     runtime.GOARCH,
